eccd115325026121b9c15dcb1a9a7d9366081c3f
Nandan Kumar <nandan@likitech.com>
2024-02-24 12:32:59 +0530

    Express Added

---
+++ includes/brands-slider.php
@@ -0,0 +1,53 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Our Brands</h3>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img  width="150" height="150" src="img/ProductImages/converse.jpg" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img width="150" height="150"  src="img/ProductImages/Wrong.png" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img width="150" height="150" src="img/ProductImages/Vans.png" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img width="150" height="150" src="img/ProductImages/puma.png" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img width="150" height="150" src="img/ProductImages/peter england.png" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img width="150" height="150" src="img/ProductImages/highlander.png" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img width="150" height="150" src="img/ProductImages/levis.png" alt="">
+					</a>	
+				</div>
+
+
+
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+		</div><!-- /.logo-slider-inner -->
+	
+</div><!-- /.logo-slider -->
